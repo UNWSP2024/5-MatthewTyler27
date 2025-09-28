a82841f559c3c0ca2d5e44b03bff26d41f381b28
MatthewTyler27 <mltyler@students.unwsp.edu>
2025-09-28 15:08:07 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,16 +1,9 @@
-# Program #1: Kilometer Converter
-# Write a program that asks the user to enter a distance in kilometers, 
-# then converts that distance to miles.  The conversion formula is as follows:  
-# Miles = kilometers x 0.6214.   
-# The conversion must be done as a function with input and output.
-
+#Matthew Tyler
+#09/28/2025
+#Kilometer Converter
 
 def kilometer_conversion(kilometers):    
-    miles = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################    
-
+    miles = kilometers * 0.6214
 
     # Return the variable to the calling function
     return miles
@@ -20,7 +13,11 @@ def kilometer_conversion(kilometers):
 #### conversion logic in the kilometer_conversion function
 if __name__ == '__main__':
     # Get User Input
-    print('in main')
+    print("This program will convert Kilometer to Miles")
+    input_kilometers = float(input("How many kilometers do you want to convert?"))
+
     # Call kilometer_conversion, don't forget to pass in the kilometer parameter!
-    
+    new_measurement = kilometer_conversion(input_kilometers)
+
     # Display the miles
+    print(format(input_kilometers, ".2f"), " Kilometers converts to: ", format(new_measurement,".2f"),"Miles")
